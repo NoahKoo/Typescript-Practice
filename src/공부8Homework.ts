03d254ab7004dfd0f55d385c9081b9e218ff08e7
Noah Koo <sjdpclsrnrk@naver.com>
2023-05-07 10:56:16 +0900

    feat(공부8Homeworks): Take a test

--- src/공부8Homework.ts
+++ src/공부8Homework.ts
@@ -1,13 +1,21 @@
-type Check = {
-  name: string;
-  phone: number;
-  email: string;
-  underAge: boolean;
+type CutType = (x: string) => string;
+
+let cutZero: CutType = function (x) {
+  let result = x.replace(/^0+/, "");
+  return result;
 };
 
-let info = {
-  name: "kim",
-  phone: 123,
-  email: "lk2s3@blahblah.com",
-  underAge: true,
+type DashType = (x: string) => number;
+
+function removeDash(x: string): number {
+  let result = x.replace(/-/g, "");
+  return parseFloat(result);
+}
+
+type NewFunc = (x: string, y: CutType, z: DashType) => {};
+
+let newFunc: NewFunc = function (x, y, z) {
+  return z(y(x));
 };
+
+newFunc("010-1111-2222", cutZero, removeDash);
